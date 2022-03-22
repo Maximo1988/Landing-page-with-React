f9dce11622a77eead287087400efaa879e989e4c
Maximo1988 <max_bd88@hotmail.com>
2022-03-22 06:39:31 +0000

    terminando ejercicio

--- src/js/component/body.js
+++ src/js/component/body.js
@@ -6,47 +6,44 @@ const Body = () => {
 	return (
 		<>
 			<Jumbotron />
-			
+
 			<div className="container">
 				<div className="row">
-					
 					<div className="col-sm-12 col-md-6 col-lg-3">
 						<Card
-							imagen= "http://www.pasarlascanutas.com/fondos%20de%20pantalla/campos_de_Ajalvir/campos_de_Ajalvir_320x240.JPG"
-							titulo= "Postal 1"
-							texto= "Some quick example text to build on the card title and make up the bulk of the cards content."
-							button= "Find Out More!"
-						/>
-					</div>
-
-					
-					<div className="col-sm-12 col-md-6 col-lg-3">
-						<Card
-							imagen= "http://www.pasarlascanutas.com/fondos%20de%20pantalla/islas%20Bahamas/islas_Bahamas_320x240.JPG"
-							titulo= "Postal 2"
-							texto= "Some quick example text to build on the card title and make up the bulk of the cards content."
-							button= "Find Out More!"
+							imagen="https://t1.ev.ltmcdn.com/es/posts/9/2/0/los_10_paisajes_mas_bonitos_de_espana_1029_orig.jpg"
+							titulo="Postal 1"
+							texto="Some quick example text to build on the card title and make up the bulk of the cards content."
+							button="Find Out More!"
 						/>
 					</div>
 
 					<div className="col-sm-12 col-md-6 col-lg-3">
 						<Card
-							imagen= "http://www.pasarlascanutas.com/fondos%20de%20pantalla/Arco_romano_Medinaceli/Arco_romano_Medinaceli_320x240.JPG"
-							titulo= "Postal 3"
-							texto= "Some quick example text to build on the card title and make up the bulk of the cards content."
-							button= "Find Out More!"
+							imagen="https://t1.ev.ltmcdn.com/es/posts/9/2/0/torcal_de_antequera_1029_3_orig.jpg"
+							titulo="Postal 2"
+							texto="Some quick example text to build on the card title and make up the bulk of the cards content."
+							button="Find Out More!"
 						/>
 					</div>
 
 					<div className="col-sm-12 col-md-6 col-lg-3">
 						<Card
-							imagen= "https://101lugaresincreibles.com/wp-content/uploads/2009/05/paisajes-hermosos.jpg"
-							titulo= "Postal 4"
-							texto= "Some quick example text to build on the card title and make up the bulk of the cards content."
-							button= "Find Out More!"
-						/>	
+							imagen="https://i.blogs.es/e32e91/trucos-enfocar-fotografia-paisaje-01/1366_2000.jpg"
+							titulo="Postal 3"
+							texto="Some quick example text to build on the card title and make up the bulk of the cards content."
+							button="Find Out More!"
+						/>
 					</div>
-					
+
+					<div className="col-sm-12 col-md-6 col-lg-3">
+						<Card
+							imagen="https://101lugaresincreibles.com/wp-content/uploads/2009/05/paisajes-hermosos.jpg"
+							titulo="Postal 4"
+							texto="Some quick example text to build on the card title and make up the bulk of the cards content."
+							button="Find Out More!"
+						/>
+					</div>
 				</div>
 			</div>
 		</>
--- src/js/component/jumbotron.js
+++ src/js/component/jumbotron.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Jumbotron = () => {
 	return (
-		<div className="jumbotron container bg-light grey px-5">
+		<div className="jumbotron container bg-lightgrey mt-3 mb-3 px-5">
 			<h1 className="display-4">A Warm Welcome!</h1>
 			<p className="lead">
 				All the Lorem Ipsum generators on the Internet tend to repeat
